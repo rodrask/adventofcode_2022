576baa4c4ed6e15796ee9699882cb0f8ace27462
rodrask <rodrask@protonmail.com>
2022-12-04 15:00:06 +0300

    day 4 solution

---
+++ day_4.rkt
@@ -0,0 +1,29 @@
+#lang racket
+(require threading)
+(require rebellion/base/range)
+
+(define (full-contain? ranges)
+  (let ([range-x (first ranges)]
+        [range-y (second ranges)])
+  (or (range-encloses? range-x range-y)
+      (range-encloses? range-y range-x))))
+
+(define (parse-job-range range-str)
+  (~> range-str (string-split _ "-") (map string->number _) (apply closed-range _)))
+
+(define (parse-jobs-pair pair-str)
+  (~> pair-str string-trim (string-split _ "," ) (map parse-job-range _)))
+
+(define (parse-part-1 in-sequence)
+    (~> in-sequence (sequence-map parse-jobs-pair _) (sequence-count full-contain?  _)))
+
+(define (parse-part-2 in-sequence)
+    (~> in-sequence (sequence-map parse-jobs-pair _) (sequence-count (lambda (rs) (apply range-overlaps? rs))  _)))
+
+(define (day-4 input parse-proc)
+  (call-with-input-file
+   input
+   (lambda~> in-lines parse-proc)))
+
+(day-4 "day_4.txt" parse-part-1)
+(day-4 "day_4.txt" parse-part-2)
